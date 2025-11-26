db2a8e2079b6c998289da833a8c0ed273f534f76
WandererMath <dengyw144@gmail.com>
2025-11-26 14:58:59 -0500

    update

--- matvec_large.cpp
+++ matvec_large.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 #define N 4096    // large matrix size
 #define LOOPS 1000  // number of iterations
 
 void matvec(float A[N][N], float x[N], float y[N]) {
+    #pragma omp parallel for
     #pragma scop
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
@@ -15,7 +17,7 @@ void matvec(float A[N][N], float x[N], float y[N]) {
 
 
 int main() {
-    clock_t start = clock();
+    double start = omp_get_wtime();
     // use static arrays instead of malloc
     static float A[N][N];
     static float x[N];
@@ -41,10 +43,13 @@ int main() {
     // for (int i = 0; i < 10; i++) printf("%f ", y[i]);
     // printf("\n");
 
-    clock_t end = clock();    // record end time
-    double total_time = (double)(end - start) / CLOCKS_PER_SEC; // in seconds
+    // clock_t end = clock();    // record end time
+    // double total_time = (double)(end - start) / CLOCKS_PER_SEC; // in seconds
 
-    printf("Total time: %f seconds\n", total_time);
+    // printf("Total time: %f seconds\n", total_time);
+
+    double end = omp_get_wtime();
+    printf("Time = %f seconds\n", end - start);
     return 0;
 }
 
